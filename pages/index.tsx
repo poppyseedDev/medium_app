bd3e90af527a3556c1d878874c3b586576049d74
Aurora Poppyseed <aurora.makovac@gmail.com>
2023-06-09 22:23:24 +0200

    basic structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link';
 import Header from '../components/Header'
 import { sanityClient, urlFor} from "../sanity";
